ec7c9eecd0cccac39a334efe7eee30d68b6ab199
Parshvi <parshvisri@gmail.com>
2022-05-30 11:40:22 +0530

    NFT Contract

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftAddress ="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const nftAddress ="0xD85D694f2bEDD493E5ce8B7Efa9af2F9aB0a420F";
 export const marketPlaceAddress="0x4Fba37A78116bfc00DeA9164DAabc3EF235be186";
